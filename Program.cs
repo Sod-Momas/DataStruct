eb8b5395ccba83237e27e6ac1f0ac8333330bb55
Sod-Momas <sothereer@gmail.com>
2019-02-14 21:54:26 +0800

    add linkedList

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DataStruct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            List<int> list = new LinkedList<int>();
+            list.add(5);
+            list.add(4);
+            list.add(3);
+            list.add(2);
+            list.add(1);
+            list.printAll();
+        }
+    }
+}
